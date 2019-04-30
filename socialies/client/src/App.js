e891375d1141a3ec05905e10a4b3180f73792b1f
Charles Cui <xarlze@ool-ad0205b7.dyn.optonline.net>
2019-04-30 15:12:04 -0400

    create initial files

---
+++ socialies/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
+import {
+
+} from './services/api-helper'
+
+function App() {
+  return (
+    <div className="App">
+      Hi
+    </div>
+  );
+}
+
+export default App;
